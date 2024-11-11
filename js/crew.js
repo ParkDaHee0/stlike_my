388388b909a7380b76c818543171e182ba5aba0a
ParkDaHee0 <dpurpose20@gmail.com>
2024-11-11 16:44:52 +0900

    Add files via upload

---
+++ js/crew.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".crew_info p").click(function () {
+    $("#dimmed").show();
+  })
+
+  $(".btn_popup02_box li").click(function () {
+    $("#dimmed").hide();
+  })
+});
---
+++ js/my.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".alarm_dimmed").click(function () {
+    $("#dimmed").show();
+  })
+
+  $(".btn_popup02_box li").click(function () {
+    $("#dimmed").hide();
+  })
+});
